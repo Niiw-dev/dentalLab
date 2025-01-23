1cba91b5cd139904ccbd18047e3d722cc17c142a
felipe.alarcon <felipe.preciado@savne.net>
2025-01-23 11:50:13 -0500

    inicio: se modifican estilos de inicio_dashboard y el 2, se continúa modificando dashboard de inicio

--- inicio/views.py
+++ inicio/views.py
@@ -22,6 +22,7 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+logger = logging.getLogger(__name__)
 
 def dd(*args):
     pdb.set_trace()
